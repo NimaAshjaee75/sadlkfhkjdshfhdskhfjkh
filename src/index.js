f17a8595b756dea0e2551c997a981a8ea2acd4e4
Nima Ashjaee <63869078+NimaAshjaee75@users.noreply.github.com>
2023-02-26 12:35:58 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Cover Letter AI</h1>
+<div>
+  Copy and Paste your resume and the job description
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
